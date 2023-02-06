63cd2737a8da596bdaf0a5540a6e92dd9663810d
Teller501 <113039777+Teller501@users.noreply.github.com>
2023-02-06 11:17:04 +0000

    added setup-samples.sh, fix-samples.sh & .gitignore

---
+++ fix-samples.sh
@@ -0,0 +1,15 @@
+cd samples/animals/
+mv countries/ ../
+
+cd ..
+cd countries/
+mv movies/ ../
+
+cd ..
+cd ..
+cd samples/animals/
+mv Monkies.txt Monkeys.txt
+
+cd ..
+mv movies/Germany.txt countries/
+mv countries/.apes.txt animals/
